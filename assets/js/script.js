3e5ee719e2bdefff60dc34081a53bfe52008b786
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-06-09 12:30:48 -0400

    updated js and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,86 @@
-// Prompts length of characters
-
-// Prompts what kind of characters to use 
-const characterCodes = Array.from(Array(26)).map( (_, i) => i + 97);
-
-const lowerCaseArr = characterCodes.map(code => String.fromCharCode(code));
-console.log(lowerCaseArr);
-
-const upperCaseArr = lowerCaseArr.map(letter => letter.toUpperCase());
-console.log(upperCaseArr)
-
-const numericArr = [1,2,3,4,5,6,7,8,9,0];
-
-const specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*']
+// target html El 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const passwordLengthResult = document.getElementById('length-result');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateBtn = document.getElementById('generate');
 
 
-// Randomizer function 
-var generatePassword = (length, hasLowercase, hasUppercase, hasNumbers, hasSpecialChar) =>{
-    var selectedChar = [
-        ...(hasLowercase ? lowerCaseArr : []),
-        ...(hasUppercase ? upperCaseArr : []),
-        ...(hasNumbers ? numericArr : []),
-        ...(hasSpecialChar? specialCharArr : []),
-    ];
 
-    let password = "";
 
-    // console.log(selectedChar);
-    // at least one character type must be selected 
-    if(selectedChar.length === 0) return "";
 
-    for(let i = 0; i < length; i++){
-        const randomizer = Math.floor(Math.random() * selectedChar.length);
-        password += selectedChar[randomizer];
+document.addEventListener("DOMContentLoaded", () => {
+    lengthEl.value = 20;
+    passwordLengthResult.innerText = 20;
+
+    let onLoadLength = passLength.value;
+    let onLoadNumbers = includeNumbers.checked;
+    let onLoadSymbols = includeSymbols.checked;
+    result.value = generatePassword(onLoadNumbers, onLoadSymbols, onLoadLength);
+  });
+
+// Eventlistener for length range
+lengthEl.addEventListener("change", (event) => {
+    passwordLengthResult.innerText = event.target.value
+});
+
+
+
+
+
+
+
+
+generateBtn.addEventListener("click", () =>{
+    const length = lengthEl.value;
+    const lowercase = lowercaseEl.checked;
+    const uppercase = uppercaseEl.checked;
+    const numbers = numbersEl.checked;
+    const symbols = symbolsEl.checked;
+    result.value = generatePassword(length, lowercase, uppercase, numbers, symbols);
+});
+function generatePassword(length, lower, upper, number, symbol) {
+  let generatedPassword = "";
+  let variationsCount = [lower, upper, number, symbol].length;
+
+  for (let i = 0; i < length; i += variationsCount) {
+    if (lower) {
+        generatedPassword += getRandomLower();
+      }
+    if (upper) {
+        generatedPassword += getRandomUpper();
+      }
+    if (number) {
+      generatedPassword += getRandomNumber();
     }
-    return password;
+    if (symbol) {
+      generatedPassword += getRandomSymbol();
+    }
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
 }
 
-console.log(generatePassword(12, false, false, false, false));
 
-// generated password is displayed
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
