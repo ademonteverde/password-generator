c6f89418666528ec648b03e6c602d80243899c75
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-06-10 00:41:35 -0400

    fixed js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,61 +10,92 @@ const generateBtn = document.getElementById('generate');
 
 
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    lengthEl.value = 20;
-    passwordLengthResult.innerText = 20;
-
-    let onLoadLength = passLength.value;
-    let onLoadNumbers = includeNumbers.checked;
-    let onLoadSymbols = includeSymbols.checked;
-    result.value = generatePassword(onLoadNumbers, onLoadSymbols, onLoadLength);
-  });
-
-// Eventlistener for length range
-lengthEl.addEventListener("change", (event) => {
-    passwordLengthResult.innerText = event.target.value
-});
-
-
-
-
-
-
-
-
-generateBtn.addEventListener("click", () =>{
-    const length = lengthEl.value;
-    const lowercase = lowercaseEl.checked;
-    const uppercase = uppercaseEl.checked;
-    const numbers = numbersEl.checked;
-    const symbols = symbolsEl.checked;
-    result.value = generatePassword(length, lowercase, uppercase, numbers, symbols);
-});
-function generatePassword(length, lower, upper, number, symbol) {
-  let generatedPassword = "";
-  let variationsCount = [lower, upper, number, symbol].length;
-
-  for (let i = 0; i < length; i += variationsCount) {
-    if (lower) {
-        generatedPassword += getRandomLower();
-      }
-    if (upper) {
-        generatedPassword += getRandomUpper();
-      }
-    if (number) {
-      generatedPassword += getRandomNumber();
-    }
-    if (symbol) {
-      generatedPassword += getRandomSymbol();
-    }
-  }
-
-  const finalPassword = generatedPassword.slice(0, length);
-
-  return finalPassword;
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
 }
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     lengthEl.value = 16;
+//     passwordLengthResult.innerText = 16;
+
+//     let onLoadLength = lengthEl.value;
+//     let onLoadLower = lowercaseEl.checked;
+//     let onLoadUpper = uppercaseEl.checked;
+//     let onLoadNumbers = numbersEl.checked;
+//     let onLoadSymbols = symbolsEl.checked;
+//     resultEl.value = generatePassword(onLoadLength, onLoadLower, onLoadUpper, onLoadNumbers, onLoadSymbols);
+//   });
+
+// // Eventlistener for length range
+// lengthEl.addEventListener("change", (event) => {
+//     passwordLengthResult.innerText = event.target.value
+// });
+
+
+
+
+
+
+
+
+generate.addEventListener("click", () =>{
+    const length = +lengthEl.value;
+    const hasLowercase = lowercaseEl.checked;
+    const hasUppercase = uppercaseEl.checked;
+    const hasNumbers = numbersEl.checked;
+    const hasSymbols = symbolsEl.checked;
+    
+    resultEl.value = generatePassword(hasLowercase, hasUppercase, hasNumbers, hasSymbols, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+
+	return finalPassword;
+}
+//   let variationsCount = [lower, upper, number, symbol].length;
+
+//   for (let i = 0; i < length; i += variationsCount) {
+//     if (lower) {
+//         generatedPassword += getRandomLower();
+//       }
+//     if (upper) {
+//         generatedPassword += getRandomUpper();
+//       }
+//     if (number) {
+//       generatedPassword += getRandomNumber();
+//     }
+//     if (symbol) {
+//       generatedPassword += getRandomSymbol();
+//     }
+//   }
+
+//   const finalPassword = generatedPassword.slice(0, length);
+
+//   return finalPassword;
+// }
 
 
 
