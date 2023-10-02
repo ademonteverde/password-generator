da8481dfca1eee0f963657c77e6815a1f9c15497
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-10-02 03:54:56 -0400

    updated css and cleaned code

--- README.md
+++ README.md
@@ -1,34 +1,30 @@
-# password-generator
-Application to generate a random password based on criteria selected.
+# Password Generator
+This application allows employees with access to sensitive data to generate a random password that meets certain criteria. The app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.
 
 ## User Story
-* AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
+As an employee with access to sensitive data, I want to randomly generate a password that meets certain criteria so that I can create a strong password that provides greater security.
 
-## Notes
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
 
-Follow these steps to complete the challenge:
-
-1. GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+## How to Use
+1. Open the index.html file in your web browser.
+2. Click the "Generate Password" button.
+3. Follow the prompts to select your password criteria.
+4. Once all prompts are answered, your secure password will be generated and displayed.
 
 ## Assets
 
+![Password Generator Demo](./assets/images/password_generator_demo.png)
+
 ### Link to GitHub Repository:
 
+[https://github.com/ademonteverde/tech_blog](https://github.com/ademonteverde/tech_blog)
 
 ### Link to deployed application:
+
+## License
+
+This project is licensed under the [MIT](https://github.com/ademonteverde/tech_blog/blob/main/LICENSE) License.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// target html El 
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
 const passwordLengthResult = document.getElementById('length-result');
@@ -8,8 +7,6 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateBtn = document.getElementById('generate');
 
-
-
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
@@ -17,101 +14,58 @@ const randomFunc = {
   symbol: getRandomSymbol
 }
 
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     lengthEl.value = 16;
-//     passwordLengthResult.innerText = 16;
-
-//     let onLoadLength = lengthEl.value;
-//     let onLoadLower = lowercaseEl.checked;
-//     let onLoadUpper = uppercaseEl.checked;
-//     let onLoadNumbers = numbersEl.checked;
-//     let onLoadSymbols = symbolsEl.checked;
-//     resultEl.value = generatePassword(onLoadLength, onLoadLower, onLoadUpper, onLoadNumbers, onLoadSymbols);
-//   });
-
-// // Eventlistener for length range
-// lengthEl.addEventListener("change", (event) => {
-//     passwordLengthResult.innerText = event.target.value
-// });
-
-
-
-
-
-
-
-
-generate.addEventListener("click", () =>{
-    const length = +lengthEl.value;
-    const hasLowercase = lowercaseEl.checked;
-    const hasUppercase = uppercaseEl.checked;
-    const hasNumbers = numbersEl.checked;
-    const hasSymbols = symbolsEl.checked;
-    
-    resultEl.value = generatePassword(hasLowercase, hasUppercase, hasNumbers, hasSymbols, length);
+generateBtn.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLowercase = lowercaseEl.checked;
+  const hasUppercase = uppercaseEl.checked;
+  const hasNumbers = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+  
+  resultEl.value = generatePassword(hasLowercase, hasUppercase, hasNumbers, hasSymbols, length);
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = "";
   const typesCount = lower + upper + number + symbol;
-	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-	
-	// Doesn't have a selected type
-	if(typesCount === 0) {
-		return '';
-	}
-	
-	// create a loop
-	for(let i=0; i<length; i+=typesCount) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
-	}
-	
-	const finalPassword = generatedPassword.slice(0, length);
-
-	return finalPassword;
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+  
+  if(typesCount === 0) {
+    return '';
+  }
+  
+  for(let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  
+  const finalPassword = shuffleString(generatedPassword.slice(0, length));
+  return finalPassword;
 }
-//   let variationsCount = [lower, upper, number, symbol].length;
-
-//   for (let i = 0; i < length; i += variationsCount) {
-//     if (lower) {
-//         generatedPassword += getRandomLower();
-//       }
-//     if (upper) {
-//         generatedPassword += getRandomUpper();
-//       }
-//     if (number) {
-//       generatedPassword += getRandomNumber();
-//     }
-//     if (symbol) {
-//       generatedPassword += getRandomSymbol();
-//     }
-//   }
-
-//   const finalPassword = generatedPassword.slice(0, length);
-
-//   return finalPassword;
-// }
-
-
 
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
 function getRandomSymbol() {
-    const symbols = "!@#$%^&*(){}[]=<>/,.";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-
-
+function shuffleString(string) {
+  const array = string.split('');
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.join('');
+}
